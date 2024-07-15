b151305d473a0287d807fbc2bce0713d30a92ac1
Ajit Singh <ajitisngh@Ajits-MacBook-Pro.local>
2024-07-15 21:49:49 +1200

    update BusStopInfo model attributes

--- app/src/main/java/com/jason/publisher/model/BusStopInfo.kt
+++ app/src/main/java/com/jason/publisher/model/BusStopInfo.kt
@@ -6,11 +6,9 @@ package com.jason.publisher.model
  * @property busStopName The name of the bus stop.
  * @property latitude The latitude coordinate of the bus stop.
  * @property longitude The longitude coordinate of the bus stop.
- * @property distanceToNextStop The distance to the next bus stop.
  */
 data class BusStopInfo(
-    val busStopName : String,
     val latitude : Double,
     val longitude : Double,
-    val distanceToNextStop : Double
+    val busStopName : String,
 )
