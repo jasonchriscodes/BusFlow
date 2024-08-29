87dd45a96757eea305005761dfad1578c142d558
Jason Christian <jasonchristian1234@gmail.com>
2024-08-30 05:37:40 +1200

    test: version 1.0.5

--- app/src/main/java/com/jason/publisher/SplashScreen.kt
+++ app/src/main/java/com/jason/publisher/SplashScreen.kt
@@ -55,7 +55,7 @@ class SplashScreen : AppCompatActivity() {
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Log.d("version name", "test version v1.0.4")
+        Log.d("version name", "test version v1.0.5")
 
         aaid = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
         sharedPrefMananger = SharedPrefMananger(this)
