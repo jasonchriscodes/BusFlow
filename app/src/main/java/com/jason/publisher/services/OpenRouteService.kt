f1d5b666ed50859a755d8c73984b3a96a3566ede
Jason Christian <jasonchristian1234@gmail.com>
2024-10-18 22:15:58 +1300

    fix: update the api url call

--- app/src/main/java/com/jason/publisher/services/OpenRouteService.kt
+++ app/src/main/java/com/jason/publisher/services/OpenRouteService.kt
@@ -50,7 +50,9 @@ class OpenRouteService {
             originLatitude: Double, originLongitude: Double,
             destinationLatitude: Double, destinationLongitude: Double
         ): String = withContext(Dispatchers.IO) {
-            val url = "https://api.openrouteservice.org/v2/directions/driving-car"
+//            val url = "https://api.openrouteservice.org/v2/directions/driving-car"
+            val url = "http://43.226.218.99:8080/ors/v2/directions/driving-car"
+
 
             // Construct the coordinates array
             val coordinates = JSONArray().apply {
