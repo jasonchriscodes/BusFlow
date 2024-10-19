60e7c3fe56b385152352c3dbb9f627e314721f66
Jason Christian <jasonchristian1234@gmail.com>
2024-10-20 05:41:27 +1300

    chore: update app to v1.0.52

--- app/src/main/java/com/jason/publisher/SplashScreen.kt
+++ app/src/main/java/com/jason/publisher/SplashScreen.kt
@@ -63,7 +63,7 @@ class SplashScreen : AppCompatActivity() {
         checkAndRequestStoragePermission()
 //        checkLocationPermission()
 
-        Log.d("version name", "test v1.0.52")
+        Log.d("version name", "v1.0.52")
         requestStoragePermissions()
         aid = getOrCreateAid()
         Log.d("Android ID", aid)
