553fb38f10c77492d423babbba982ec1a2d5fde1
Jason Christian <jasonchristian1234@gmail.com>
2025-11-02 07:09:58 +1300

    chore: prevent app from lock screen while open

--- app/src/main/java/com/jason/publisher/App.kt
+++ app/src/main/java/com/jason/publisher/App.kt
@@ -1,13 +1,54 @@
-// App.kt
 package com.jason.publisher
 
+import android.app.Activity
 import android.app.Application
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
 import com.jason.publisher.main.utils.FileLogger
 
-class App : Application() {
+class App : Application(), Application.ActivityLifecycleCallbacks {
     override fun onCreate() {
         super.onCreate()
         FileLogger.init(this)
+        registerActivityLifecycleCallbacks(this)
     }
-}
 
+    // Apply to every Activity while it's visible
+    override fun onActivityResumed(activity: Activity) {
+        activity.window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            activity.setShowWhenLocked(true)
+            activity.setTurnScreenOn(true)
+        } else {
+            @Suppress("DEPRECATION")
+            activity.window.addFlags(
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+            )
+        }
+    }
+
+    // Clear when no longer visible
+    override fun onActivityPaused(activity: Activity) {
+        activity.window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            activity.setShowWhenLocked(false)
+            activity.setTurnScreenOn(false)
+        } else {
+            @Suppress("DEPRECATION")
+            activity.window.clearFlags(
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+            )
+        }
+    }
+
+    // Unused callbacks
+    override fun onActivityCreated(a: Activity, s: Bundle?) {}
+    override fun onActivityStarted(a: Activity) {}
+    override fun onActivityStopped(a: Activity) {}
+    override fun onActivitySaveInstanceState(a: Activity, o: Bundle) {}
+    override fun onActivityDestroyed(a: Activity) {}
+}
